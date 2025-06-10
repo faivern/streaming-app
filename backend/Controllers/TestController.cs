4b4eced60d077c9f79e196f0b0f62e2aac5ac4d7
Gustaf Faivre <gustfaiv100@student.kau.se>
2025-06-10 02:14:26 +0200

    Initial commit

---
+++ backend/Controllers/TestController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace backend.Controllers
+{
+
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TestController : Controller
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(new { message = "Hello, World!" });
+        }
+    }
+}
