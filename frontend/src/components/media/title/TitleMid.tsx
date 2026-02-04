46b002fef180f0b7faf697d41c858c1662292f4c
Gustaf Faivre <gustfaiv100@student.kau.se>
2026-02-04 17:11:24 +0100

    wcag fixes to mediadetailpage

--- frontend/src/components/media/title/TitleMid.tsx
+++ frontend/src/components/media/title/TitleMid.tsx
@@ -7,9 +7,9 @@ type TitleMidProps = {
 
 const TitleMid = ({ children, className }: TitleMidProps) => {
   return (
-    <div className="flex flex-row items-stretch mb-4 ">
-      <span className="mr-2 bg-gradient-to-b from-accent-primary to-accent-secondary w-1 rounded"></span>
-      <h2 className={`text-2xl font-semibold ${className}`}>
+    <div className="flex flex-row items-stretch mb-6">
+      <span className="mr-3 bg-gradient-to-b from-accent-primary to-accent-secondary w-1 rounded"></span>
+      <h2 className={`text-2xl font-bold text-text-h1 ${className}`}>
         {children}
       </h2>
     </div>
