74fc1bd3ffcbea03c3b80e425b16408b17b2cc84
Gustaf Faivre <gustfaiv100@student.kau.se>
2026-02-08 19:41:57 +0100

    minor fixes

--- frontend/src/components/media/carousel/MediaCastCarousel.tsx
+++ frontend/src/components/media/carousel/MediaCastCarousel.tsx
@@ -1,28 +1,31 @@
-import MediaCastCard from '../cards/MediaCastCard.tsx';
-import { Link, useParams } from 'react-router-dom';
+import MediaCastCard from "../cards/MediaCastCard.tsx";
+import { Link, useParams } from "react-router-dom";
 import "../../../style/TitleHover.css";
-import type { Credit } from '../../../types/tmdb.ts';
+import type { Credit } from "../../../types/tmdb.ts";
+import TitleMid from "../title/TitleMid.tsx";
 
 type Props = {
   cast: Credit[];
-}
+};
 
 const MediaCastCarousel = ({ cast }: Props) => {
-    const { media_type, id } = useParams<{ media_type: string; id: string }>();
+  const { media_type, id } = useParams<{ media_type: string; id: string }>();
 
-    const mediaCastStart = 0;
-    const mediaCastEnd = 12;
+  const mediaCastStart = 0;
+  const mediaCastEnd = 12;
 
-    return (
+  return (
     <section className="w-full py-4 px-4 md:px-12 max-w-7xl mx-auto">
-      <div className="flex justify-between items-center mb-6">
-        <h2 className="text-2xl font-bold text-text-h1">Cast & Crew</h2>
-          <Link to={`/media/${media_type}/${id}/credits`}>
-            <span className="underline-hover">
-              Explore all
-              <span className="underline-bar"></span>
-            </span>
-          </Link>
+      <div className="flex justify-between items-center mb-4">
+        <div className="[&>div]:mb-0">
+          <TitleMid>Cast & Crew</TitleMid>
+        </div>
+        <Link to={`/media/${media_type}/${id}/credits`}>
+          <span className="underline-hover text-gray-400">
+            Explore all
+            <span className="underline-bar"></span>
+          </span>
+        </Link>
       </div>
 
       <div
@@ -32,14 +35,11 @@ const MediaCastCarousel = ({ cast }: Props) => {
         aria-label="Cast and crew carousel"
       >
         {cast.slice(mediaCastStart, mediaCastEnd).map((person) => (
-          <MediaCastCard
-            key={person.id}
-            cast={person}
-          />
+          <MediaCastCard key={person.id} cast={person} />
         ))}
       </div>
     </section>
-    )
-}
+  );
+};
 
-export default MediaCastCarousel
+export default MediaCastCarousel;
