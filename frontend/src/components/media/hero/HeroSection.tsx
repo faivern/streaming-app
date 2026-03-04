715a35be63bf711070c7b9683a015fd48a15dfc3
faivern <gustaf.faivre@yahoo.com>
2026-03-04 22:46:58 +0100

    changes

--- frontend/src/components/media/hero/HeroSection.tsx
+++ frontend/src/components/media/hero/HeroSection.tsx
@@ -6,25 +6,19 @@ type Props = {
 
 const HeroSection = ({ total_results }: Props) => {
   const durationTimeSeconds = 4;
-  const separatorSymbol = " ";
+  const separatorSymbol = ",";
 
   return (
     <div className="text-center mb-8 py-4">
       <h1 className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl font-bold text-text-h1">
-        Your Personal Cinema Atlas
-      </h1>
-      <p className="mt-3 text-xl sm:text-2xl md:text-3xl lg:text-4xl font-bold">
+        Cinema Atlas {" "}
         <span className="text-transparent bg-clip-text bg-gradient-to-r from-accent-primary to-accent-secondary">
           <CountUp end={total_results} duration={durationTimeSeconds} separator={separatorSymbol} />
         </span>{" "}
         <span className="text-text-h1">Movies & Shows</span>
-      </p>
+      </h1>
       <p className="mt-2 text-sm sm:text-base md:text-lg lg:text-xl font-medium text-text-h1/60">
-        Discover what to watch, track everything you've seen, and find where
-        it's streaming.
-      </p>
-      <p className="mt-3 text-sm sm:text-base md:text-lg font-semibold tracking-wide text-transparent bg-clip-text bg-gradient-to-r from-accent-primary to-accent-secondary uppercase">
-        Start Exploring
+        Explore, discover, and track movies & shows — and find where to stream them.
       </p>
     </div>
   );
